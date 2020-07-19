48bde621075fd1d9d826abb2ad923be2100d30a3
ivanluchkin <id.luchkin@gmail.com>
2020-07-19 10:51:11 +0300

    project finished

--- CurrencyConverter/ViewController.m
+++ CurrencyConverter/ViewController.m
@@ -6,12 +6,43 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+
+@interface ViewController () <CRCurrencyRequestDelegate>
+
+@property (nonatomic) CRCurrencyRequest *request;
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
+@property (weak, nonatomic) IBOutlet UILabel *currencyPLN;
+@property (weak, nonatomic) IBOutlet UILabel *currencyJPY;
+@property (weak, nonatomic) IBOutlet UILabel *currencyGBP;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)buttonTapped:(id)sender {
+    self.convertButton.enabled = NO;
+    
+    self.request = [[CRCurrencyRequest alloc] init];
+    self.request.delegate = self;
+    [self.request start];
+}
+
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies {
+    self.convertButton.enabled = YES;
+    double inputValue = [self.inputField.text floatValue];
+   
+    self.currencyPLN.text = [NSString stringWithFormat:@"%.2f", inputValue * currencies.PLN];
+    
+    self.currencyJPY.text = [NSString stringWithFormat:@"%.2f", inputValue * currencies.JPY];
+    
+    self.currencyGBP.text = [NSString stringWithFormat:@"%.2f", inputValue * currencies.GBP];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
